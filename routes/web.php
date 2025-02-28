5aadd29b0626423a25679772a685e6f53d36fe3e
JOSTINUPFIM <2218031447@upfim.edu.mx>
2025-02-28 11:29:02 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,39 @@ Route::get('/', function () {
 Route::get('/inicio', function () {
     return view('inicio');
 });
+Route::get('/tephe', function () {
+    return view('tephe');
+});
+Route::get('/heredad', function () {
+    return view('heredad');
+});
+Route::get('/paraiso', function () {
+    return view('paraiso');
+});
+Route::get('/grutas', function () {
+    return view('grutas');
+});
+Route::get('/pueblonuevo', function () {
+    return view('pueblonuevo');
+});
+Route::get('/tollan', function () {
+    return view('tollan');
+});
+Route::get('/madho', function () {
+    return view('madho');
+});
+Route::get('/cuevitas', function () {
+    return view('cuevitas');
+});
+Route::get('/maguey_blanco', function () {
+    return view('maguey_blanco');
+});
+Route::get('/te_pathe', function () {
+    return view('te_pathe');
+});
+Route::get('/1eco_alberto', function () {
+    return view('1eco_alberto');
+});
+Route::get('/dauthi', function () {
+    return view('dauthi');
+});
