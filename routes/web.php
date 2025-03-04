d3d955f49ffa575108cdf7bf51f21675d3c721a0
rocioperez071001 <134241632+rocioperez071001@users.noreply.github.com>
2025-03-04 10:51:40 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,56 +2,87 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/inicio', function () {
     return view('inicio');
-});
+})->name('inicio');
+
 Route::get('/tephe', function () {
     return view('tephe');
-});
+})->name('tephe');
+
 Route::get('/heredad', function () {
     return view('heredad');
-});
+})->name('heredad');
+
 Route::get('/paraiso', function () {
     return view('paraiso');
-});
+})->name('paraiso');
+
 Route::get('/grutas', function () {
     return view('grutas');
-});
+})->name('grutas');
+
 Route::get('/pueblonuevo', function () {
     return view('pueblonuevo');
-});
+})->name('pueblonuevo');
+
 Route::get('/tollan', function () {
     return view('tollan');
-});
+})->name('tollan');
+
 Route::get('/madho', function () {
     return view('madho');
-});
+})->name('madho');
+
 Route::get('/cuevitas', function () {
     return view('cuevitas');
-});
+})->name('cuevitas');
+
 Route::get('/maguey_blanco', function () {
     return view('maguey_blanco');
-});
+})->name('maguey_blanco');
+
 Route::get('/te_pathe', function () {
     return view('te_pathe');
-});
-Route::get('/1eco_alberto', function () {
-    return view('1eco_alberto');
-});
+})->name('te_pathe');
+
+Route::get('/ecoalberto', function () {
+    return view('ecoalberto');
+})->name('ecoalberto');
+
 Route::get('/dauthi', function () {
     return view('dauthi');
-});
+})->name('dauthi');
+
+Route::get('/diospadre', function () {
+    return view('diospadre');
+})->name('diospadre');
+
+Route::get('/taxadho', function () {
+    return view('taxadho');
+})->name('taxadho');
+
+Route::get('/lagranja', function () {
+    return view('lagranja');
+})->name('lagranja');
+
+Route::get('/elmanantial', function () {
+    return view('elmanantial');
+})->name('elmanantial');
+
+Route::get('/lagloria', function () {
+    return view('lagloria');
+})->name('lagloria');
+
+Route::get('/Tlaco', function () {
+    return view('Tlaco');
+})->name('Tlaco');
+
+Route::get('/presamadho', function () {
+    return view('presamadho');
+})->name('presamadho');
+
