7c2d99728d761e681de75d2833a6fda2d199c6b3
rocioperez071001 <134241632+rocioperez071001@users.noreply.github.com>
2025-03-04 08:11:36 -0600

    Update inicio.blade.php

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio - Cielos Termales</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
 
@@ -31,64 +31,72 @@
     <p>Ixmiquilpan es también un importante centro artesanal en donde se pueden adquirir textiles de lana, de fibra de maguey o bordados diversos; piezas de madera con figuras de concha de abulón incrustadas y muebles de carrizo.</p>
     <p>Su cocina tradicional se compone especialmente de platillos hechos con ingredientes nativos, como las flores e insectos comestibles. También está su conocida barbacoa, que destaca por el método de preparación y la carne procedente de ganado de pastoreo.</p>
     <div class="collage">
-      <a href="te-pathe.php" class="collage-item">
-        <img src="img/te-pathe.jpg" alt="Te-Pathe">
+      <a href="{{ route('te_pathe') }}" class="collage-item">
+        <img src="{{ asset('img/te-pathe.jpg') }}" alt="Te-Pathe">
         <div class="collage-caption">TE-PATHE</div>
       </a>
 
-      <a href="1eco_alberto.php" class="collage-item">
-        <img src="img/ecoalberto.jpg" alt="Eco Alberto">
+      <a href="{{ route('ecoalberto') }}" class="collage-item">
+        <img src="{{ asset('img/ecoalberto.jpg') }}" alt="Eco Alberto">
         <div class="collage-caption">ECO ALBERTO</div>
       </a>
 
-      <a href="dios-padre.html" class="collage-item">
-        <img src="img/diospadre.jpg" alt="Dios Padre">
+      <a href="{{ route('diospadre') }}" class="collage-item">
+        <img src="{{ asset('img/diospadre.jpg') }}" alt="Dios Padre">
         <div class="collage-caption">DIOS PADRE</div>
       </a>
       
-      <a href="maguey-blanco.html" class="collage-item">
-        <img src="img/maguey.jpg" alt="Maguey Blanco">
+      <a href="{{ route('maguey_blanco') }}" class="collage-item">
+        <img src="{{ asset('img/maguey.jpg') }}" alt="Maguey Blanco">
         <div class="collage-caption">MAGUEY BLANCO</div>
       </a>
 
-      <a href="pueblo-nuevo.html" class="collage-item">
-        <img src="img/pueblonuevo.jpg" alt="Pueblo Nuevo">
+      <a href="{{ route('pueblonuevo') }}" class="collage-item">
+        <img src="{{ asset('img/pueblonuevo.jpg') }}" alt="Pueblo Nuevo">
         <div class="collage-caption">PUEBLO NUEVO</div>
       </a>
 
-      <a href="la-heredad.html" class="collage-item">
-        <img src="img/laheredad.jpg" alt="La Heredad">
+      <a href="{{ route('heredad') }}" class="collage-item">
+        <img src="{{ asset('img/laheredad.jpg') }}" alt="La Heredad">
         <div class="collage-caption">LA HEREDAD</div>
       </a>
 
-      <a href="dauthi.html" class="collage-item">
-        <img src="img/dauthi.jpg" alt="Dauthi">
+      <a href="{{ route('dauthi') }}" class="collage-item">
+        <img src="{{ asset('img/dauthi.jpg') }}" alt="Dauthi">
         <div class="collage-caption">DAUTHI</div>
       </a>
 
-      <a href="tephe.html" class="collage-item">
-        <img src="img/tephe.jpg" alt="Tephe">
-        <div class="collage-caption">TEPHE</div>
-      </a>
+    <a href="{{ route('tephe') }}" class="collage-item">
+      <img src="{{ asset('img/tephe.jpg') }}" alt="Tephe">
+      <div class="collage-caption">TEPHE</div>
+    </a>
 
-      <a href="taxadho.html" class="collage-item">
-        <img src="img/taxadho.jpg" alt="Taxadhó">
+      <a href="{{ route('taxadho') }}" class="collage-item">
+        <img src="{{ asset('img/taxadho.jpg') }}" alt="Taxadhó">
         <div class="collage-caption">TAXADHÓ</div>
       </a>
 
-      <a href="lagranja.html" class="collage-item">
-        <img src="img/lagranja.jpg" alt="La Granja">
+      <a href="{{ route('lagranja') }}" class="collage-item">
+        <img src="{{ asset('img/lagranja.jpg') }}" alt="La Granja">
         <div class="collage-caption">LA GRANJA</div>
       </a>
 
-      <a href="el-manantial.html" class="collage-item">
-        <img src="img/elmanantial.jpg" alt="El Manantial">
+      <a href="{{ route('elmanantial') }}" class="collage-item">
+        <img src="{{ asset('img/elmanantial.jpg') }}" alt="El Manantial">
         <div class="collage-caption">EL MANANTIAL</div>
       </a>
 
-      <a href="las-cuevitas.html" class="collage-item">
-        <img src="img/lascuevitas.jpg" alt="Las Cuevitas">
+      <a href="{{ route('cuevitas') }}" class="collage-item">
+        <img src="{{ asset('img/lascuevitas.jpg') }}" alt="Las Cuevitas">
         <div class="collage-caption">LAS CUEVITAS</div>
+      </a>
+      <a href="{{ route('tollan') }}" class="collage-item">
+        <img src="{{ asset('img/tollan.jpg') }}" alt="Las Cuevitas">
+        <div class="collage-caption">TOLLÁN</div>
+      </a>
+      <a href="{{ route('paraiso') }}" class="collage-item">
+        <img src="{{ asset('img/valle.jpg') }}" alt="Las Cuevitas">
+        <div class="collage-caption">VALLE PARAÍSO</div>
       </a>
     </div>
   </div>
@@ -102,13 +110,13 @@
     <p>Este sensacional lugar es famoso por sus artesanías de ixtle, fibra de maguey y lechuguilla, como son los ayates, así como jarciarías como las conocidas escobetillas de uso doméstico; además, existe el uso del telar de cintura y los tradicionales bordados otomíes en lana o algodón.</p>
     <p>En cuanto a la gastronomía, se producen y consumen múltiples derivados de maguey como los chinicuiles, los gualumbos, el jarabe de agave y el pan de pulque.</p>
     <div class="collage">
-      <a href="grutas.html" class="collage-item">
-        <img src="img/grutas.jpg" alt="Grutas">
+      <a href="{{ route('grutas') }}" class="collage-item">
+        <img src="{{ asset('img/grutas.jpg') }}" alt="Grutas">
         <div class="collage-caption">GRUTAS DE TOLANTONGO</div>
       </a>
 
-      <a href="llamas.html" class="collage-item">
-        <img src="img/gloria.jpeg" alt="Gloria">
+      <a href="{{ route('laglo') }}" class="collage-item">
+        <img src="{{ asset('img/gloria.jpeg') }}" alt="Gloria">
         <div class="collage-caption">LA GLORIA</div>
       </a>
     </div>
@@ -123,8 +131,8 @@
     <p>En cuanto a patrimonio inmaterial, Chilcuautla preserva artesanías como las hechas con fibra de maguey como los ayates, los tejidos en telar de cintura o los bordados en tela de algodón propios de la indumentaria tradicional indígena. De sus fiestas tradicionales, el Carnaval hace gala con la presencia de personas ataviadas de xitás, que en hñäñhu son los viejitos, cuyo disfraz está hecho de fibras y sus máscaras de penca de maguey.</p>
     <p>En cuanto a la tradición gastronómica, existen usos comunes con el resto del Valle del Mezquital en cuanto a la abundancia de preparaciones con tuna, nopal, xoconostle o insectos, a lo que se añade el uso de ingredientes como la granada que se cultiva en la zona, así como pescados y quesos que ahí se producen.</p>
     <div class="collage">
-      <a href="tlaco.html" class="collage-item">
-        <img src="img/tlaco.jpeg" alt="Tlaco">
+      <a href="{{ route('tlaco') }}" class="collage-item">
+        <img src="{{ asset('img/tlaco.jpeg') }}" alt="Tlaco">
         <div class="collage-caption">TLACO</div>
       </a>
     </div>
@@ -134,14 +142,9 @@
 <div class="container">
   <h4>Alfajayucan</h4>
   <p>Alfajayucan tiene un territorio conformado por lomeríos y un clima cálido. Posee múltiples presas y embalses de agua construidos para irrigar las tierras de cultivo de la región. En estos cuerpos de agua se produce mojarra, tilapia y carpa, y en ciertas temporadas las aves migratorias surcan el cielo para llegar ahí a reproducirse.</p>
-  <p>Se ubica en una comarca agrícola de regadío, habitada por pueblos otomíes que conjugan actividades como la cosecha de múltiples alimentos con labores artesanales. De su patrimonio cultural edificado, resalta el antiguo Convento de San Martín Obispo, obra franciscana del siglo XVI que en uno de sus altares tiene una imagen del Señor de la Buena Muerte, un Cristo crucificado alabado desde hace siglos por toda la población. Otra joya de este conjunto es la cruz atrial, escultura de piedra ubicada en el centro del patio del convento, que con diversos símbolos narra el origen del cristianismo.</p>
-  <p>La riqueza del patrimonio cultural inmaterial nos muestra artesanías, entre las que destaca el sombrero otomí, además de la fabricación de otros productos de palma tejida como los petates y la elaboración de ayates de ixtle. De sus fiestas tradicionales son atractivos los carnavales, con la presencia de los xitas o “viejitos”, personajes que se enfrentan en una guerra florida arrojándose naranjas. En su gastronomía hay desde tamales tradicionales, panes como los cocoles de ajonjolí, antojitos como los tlacoyos rellenos de frijol y los chicharrones de res, que son las vísceras fritas en un cazo con manteca.</p>
-  <div class="collage">
-    <a href="presa.html" class="collage-item">
-      <img src="img/presa.jpg" alt="Presa">
-      <div class="collage-caption">PRESA MADHÓ</div>
-    </a>
-  </div>
+  <p>Se ubica en una comarca agrícola de regadío, habitada por pueblos otomíes que conjugan actividades como la cosecha de múltiples alimentos con labores artesanales. De su patrimonio cultural edificado, resalta el antiguo Convento de San Martín Obispo, obra franciscana del siglo XVI que en uno de sus altares tiene una imagen del Señor de la Buena Muerte, un Cristo crucificado alabado desde hace siglos por toda la población. Otra joya de este conjunto es la cruz atrial, escultura de piedra ubicada en el centro del patio del convento, que con su tamaño y estructura majestuosa es testigo de la importancia histórica de Alfajayucan.</p>
+  <p>En cuanto a la gastronomía, destaca la tradición de preparar tamales de pollo y cocoles de anís o de calabaza.</p>
 </div>
+
 </body>
 </html>
