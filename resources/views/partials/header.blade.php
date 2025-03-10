f5d238e43aafbfd0e84aac94896671df293ac11f
rocioperez071001 <134241632+rocioperez071001@users.noreply.github.com>
2025-03-09 23:15:05 -0600

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,17 @@
-<nav class="bg-blue-500 p-4">
-    <div class="container mx-auto flex justify-between items-center">
-        <a href="{{ url('/') }}" class="text-white text-lg font-bold">Balnearios</a>
-        <ul class="flex space-x-4">
-            <li><a href="{{ url('/locaciones') }}" class="text-white hover:underline">Locaciones</a></li>
-            <li><a href="{{ url('/servicios') }}" class="text-white hover:underline">Servicios</a></li>
-            <li><a href="{{ url('/contacto') }}" class="text-white hover:underline">Contacto</a></li>
-        </ul>
+<header class="main-header">
+    <div class="header-top">
+        <h1 class="logo">Cielos termales</h1>
+        <div class="search-bar">
+            <input type="text" placeholder="Buscar...">
+            <i class="fas fa-search"></i>
+        </div>
     </div>
-</nav>
+    <nav class="main-nav">
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Locaciones</a></li>
+            <li><a href="#">Regresar</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
