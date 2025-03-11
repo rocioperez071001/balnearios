f4e735fa8a60c05ad85ace357528b629463b8fd8
FranciscoSZ-21 <2218031499@upfim.edu.mx>
2025-03-11 08:19:09 -0600

    Update taxadho.blade.php

--- resources/views/taxadho.blade.php
+++ resources/views/taxadho.blade.php
@@ -5,24 +5,24 @@
 @section('content')
      <div class="container">
         <main>
-        <div class="balneario">
-                <img src="img/taxadho-logo.png" alt="Balneario Taxadho">
-                <h1>Balneario taxadho</h1>
+        <div class="parque">
+                <img src="img/taxadho-logo.png" alt="Parque Ecoturístico Taxadhó">
+                <h1>Parque Ecoturístico Taxadhó</h1>
             </div>
 
             <section class="description">
-                <p>El Balneario taxadho es un destino turístico ideal para disfrutar de aguas termales, áreas recreativas y un ambiente familiar en el corazón de Hidalgo. Es perfecto para relajarse y divertirse en sus múltiples atracciones.</p>
+                <p>El Parque Ecoturístico Taxadhó es un destino ideal para disfrutar de la naturaleza, actividades al aire libre y un ambiente familiar en el corazón de Hidalgo. Es perfecto para relajarse y vivir aventuras en un entorno ecológico.</p>
                 
                 <h3>Atracciones:</h3>
 
-                <h3>Piscinas de Aguas Termales</h3>
-                <p>Relájate en nuestras piscinas con aguas termales cristalinas, conocidas por sus propiedades terapéuticas.</p>
+                <h3>Senderos y Caminatas</h3>
+                <p>Explora nuestros senderos rodeados de naturaleza, ideales para caminatas y avistamiento de flora y fauna.</p>
 
-                <h3>Toboganes y Juegos Acuáticos</h3>
-                <p>Diviértete con nuestros emocionantes toboganes y juegos acuáticos diseñados para todas las edades.</p>
+                <h3>Tirolesa y Deportes de Aventura</h3>
+                <p>Vive la adrenalina con nuestra tirolesa y otras actividades de aventura diseñadas para todas las edades.</p>
 
                 <h3>Zona de Camping y Cabañas</h3>
-                <p>Disfruta de una experiencia única en nuestra zona de camping o alójate en nuestras cómodas cabañas rústicas.</p>
+                <p>Disfruta de una experiencia única en nuestra zona de camping o alójate en nuestras cómodas cabañas ecológicas.</p>
 
                 <h3>Áreas de Picnic y Restaurante</h3>
                 <p>Relájate y comparte momentos especiales en nuestras áreas de picnic, o prueba los mejores platillos en nuestro restaurante.</p>
@@ -38,10 +38,10 @@
                 <h3>Contacto</h3>
                 <p>Abierto los 365 días del año.  
                 Oficina de 9:00 a.m. a 8:00 p.m. de lunes a domingo.  
-                <a href="mailto:contacto@balneariotaxadho.com">contacto@balneariotaxadho.com</a></p>
+                <a href="mailto:contacto@parqueecoturisticotaxadho.com">contacto@parqueecoturisticotaxadho.com</a></p>
             </section>
 
             <a href="{{ route('servicios') }}" class="servicios-btn">Servicios</a>
         </main>
     </div>
-    @endsection
+@endsection
