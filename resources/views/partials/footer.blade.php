cb02877e809ab7b360d94707f523130757f04f49
rocioperez071001 <134241632+rocioperez071001@users.noreply.github.com>
2025-03-20 07:24:59 -0600

    Update footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="custom-footer">
+    <footer>
+        <div class="social-icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-facebook-messenger"></i></a>
+            <a href="#"><i class="fab fa-pinterest"></i></a>
+            <a href="#"><i class="fas fa-globe"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+        </div>
+        <p>© Cielos.Termales 2025</p>
+    </footer>
+</div>
